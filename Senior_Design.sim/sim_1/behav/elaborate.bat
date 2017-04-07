f8aefb78f3663fb9b6e5fd3a57f44c12a5d1572b
Keon <Keon>
2017-04-07 13:14:49 -0500

    Added missing comments, changed the clk_divider.vhd file name to FIFO_write_clk_generator.vhd as it is more appropriate.
    Added a secondary project (Senior_Design_Test) which autogenerates input for board testing. Significantly condensed down the FIFO logic, as it now utilises modulus arithmetic rather than nested if-else statements. Now it also implicitely accounts for possible premature read ptr - write ptr collisions (in the case of fast reads) without the need for a read_synchroniser. See the documentation in FIFO.vhd for details.

--- Senior_Design.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xelab  -wto 61dc3ce03d3940c790a07b47870f2c95 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot logic_analyser_tb_behav xil_defaultlib.logic_analyser_tb -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- Senior_Design.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xsim logic_analyser_tb_behav -key {Behavioral:sim_1:Functional:logic_analyser_tb} -tclbatch logic_analyser_tb.tcl -view C:/Users/Jay/Dropbox/Senior_Design/FIFO_tb_behav.wcfg -view C:/Users/Jay/Dropbox/Senior_Design/LAnalyser_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
